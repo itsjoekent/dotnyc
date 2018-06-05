07347d66e3533c6fc6f20c40d2a0dd25e977b0e0
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2018-06-05 12:37:42 -0400

    Bootstrap stuff

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,3 +1,33 @@
-// build client
-// copy build output to assets folder
-// exit process
+const fs = require('fs');
+const path = require('path');
+const cmd = require('node-cmd');
+
+const CLIENT_PATH = path.join(__dirname, '../client');
+const ASSETS_PATH = path.join(__dirname, '/assets');
+
+const pipeline = `
+  cp -R ${CLIENT_PATH}/* ${ASSETS_PATH} &&
+  cd ${ASSETS_PATH} &&
+  npm install &&
+  npm run build
+`;
+
+const pipelineProcess = cmd.get(pipeline, (err, data, stderr) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Bootstrapped app successfully!');
+  }
+
+  process.exit();
+});
+
+let log = '';
+pipelineProcess.stdout.on('data', (data) => {
+  log += data;
+
+  if (log[log.length - 1] === '\n') {
+    console.log(log);
+    log = '';
+  }
+});
