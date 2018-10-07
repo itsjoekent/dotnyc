e10243e87168884422f9f8029faf6e57b0e4208b
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2018-10-07 14:52:20 -0400

    boom. 💥

--- client/src/components/AsyncEditor.js
+++
@@ -1,5 +0,0 @@
-import SplitComponent from './SplitComponent';
-
-const AsyncEditor = SplitComponent('Editor', false);
-
-export default AsyncEditor;
--- client/src/routes.js
+++
@@ -1,4 +0,0 @@
-export const HOME_PATH = '/';
-export const DIRECTORY_PATH = '/www';
-export const AUTH_PATH = '/p@$$word';
-export const CONTENT_PATH = '/:hex/:alias';
