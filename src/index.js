27b54dacf2f637b0e3ef4fec8dcd4a85c767132d
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-05-18 11:07:33 -0400

    Fresh

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
-import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import GlobalStyle from './GlobalStyle';
-import theme from './theme';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render((
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <React.Fragment>
-        <GlobalStyle />
-        <App />
-      </React.Fragment>
-    </BrowserRouter>
-  </ThemeProvider>
-), document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
