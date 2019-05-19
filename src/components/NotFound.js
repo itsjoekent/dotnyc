66a027bc9c7bd556d62b7e5b6c34524f0cd16249
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-05-18 23:23:56 -0400

    404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 80vh;
+`;
+
+const Header = styled.h1`
+  color: ${({ theme }) => theme.colors.blue[600]};
+
+  text-transform: uppercase;
+
+  margin-bottom: ${({ theme }) => theme.spacing.base}px;
+
+  font-family: ${({ theme }) => theme.font.family};
+  font-size: ${({ theme }) => theme.font.size.hero}px;
+  font-weight: ${({ theme }) => theme.font.weight.dark};
+`;
+
+const Gif = styled.img`
+  max-width: 400px;
+`;
 
 function NotFound(props) {
   return (
-    <h1>not found</h1>
+    <NotFoundContainer>
+      <Header>page not found</Header>
+      <Gif src={process.env.PUBLIC_URL + '/pizza-rat.gif'} alt="NYC Pizza rat" />
+    </NotFoundContainer>
   );
 }
 
