094378f6b08fd1d2c723f50713ab25506602ddbc
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2020-09-08 18:03:08 -0400

    Support scss extension for syntax highlighting, renaming home.css

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(scss|css)$/,
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
